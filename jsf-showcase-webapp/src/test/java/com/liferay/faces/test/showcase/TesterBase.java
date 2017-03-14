4a95b0c46008eec3387bf81e219a11542ce7825d
pcwhite <philip.white@liferay.com>
2017-03-14 11:01:37 -0400

    FACES-2999 Develop tests for the Data category of the Alloy Showcase

--- jsf-showcase-webapp/src/test/java/com/liferay/faces/test/showcase/TesterBase.java
+++ jsf-showcase-webapp/src/test/java/com/liferay/faces/test/showcase/TesterBase.java
@@ -1,4 +1,19 @@
 /**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,9 +41,11 @@ import java.util.logging.Logger;
 import org.junit.Assert;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 import com.liferay.faces.test.selenium.Browser;
 import com.liferay.faces.test.selenium.IntegrationTesterBase;
@@ -65,6 +82,9 @@ public class TesterBase extends IntegrationTesterBase {
 
 	// Private Constants
 	private static final String CONTAINER = TestUtil.getContainer("tomcat");
+	private static final String FIRE_SELECT_CHANGE_EVENT_SCRIPT =
+		"var changeEvent = document.createEvent('HTMLEvents');" +
+		"changeEvent.initEvent('change', true, true); arguments[0].parentNode.dispatchEvent(changeEvent);";
 	private static final boolean SIGN_IN;
 
 	static {
@@ -106,6 +126,47 @@ public class TesterBase extends IntegrationTesterBase {
 				image);
 		Assert.assertTrue("Image " + imageXpath + " (src=\"" + imageSrc + "\") is not rendered in the DOM.",
 			imageRendered);
+	}
+
+	/**
+	 * Click an option and wait for Ajax to rerender the option. This method exists because {@link
+	 * Browser#clickAndWaitForAjaxRerender(java.lang.String)} does not work on selectOneMenu, selectManyListbox, and
+	 * SelectManyMenu. For more information see method comments.
+	 */
+	protected void clickOptionAndWaitForAjaxRerender(Browser browser, String optionXpath) {
+
+		// some tests occasionally fail due to elements moving off screen, so center the element in the view.
+		if (this.getClass().getName().contains("Menu") || this.getClass().getName().contains("DataTable")) {
+			browser.centerElementInView(optionXpath);
+		}
+
+		WebElement optionElement = browser.findElementByXpath(optionXpath);
+
+		// Note: clicking a selectOneMenu option on Chrome and PhantomJS via Actions.click(WebElement) (which is used
+		// by Browser.clickAndWaitForAjaxRerender(String)) does not fire the select element's change event, so the
+		// element must be clicked via Element.click().
+		optionElement.click();
+
+		// phantomjs browser does not fire a change event when a <select multiple="multiple"> <option> is clicked, so
+		// the event must be fired manually.
+		if ("phantomjs".equals(browser.getName())) {
+
+			try {
+
+				WebElement selectElement = optionElement.findElement(By.xpath(".."));
+				Select select = new Select(selectElement);
+
+				if (select.isMultiple()) {
+					browser.executeScript(FIRE_SELECT_CHANGE_EVENT_SCRIPT, optionElement);
+				}
+			}
+			catch (StaleElementReferenceException e) {
+				// do nothing. The element is stale because an ajax rerender has correctly occured.
+			}
+		}
+
+		browser.waitUntil(ExpectedConditions.stalenessOf(optionElement));
+		browser.waitForElementVisible(optionXpath);
 	}
 
 	@Override
